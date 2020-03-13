669d0a62eb49976e1da434f4c1efd0d3db00ad0b
Luis De Anda <ldeandac16@gmail.com>
2020-03-13 10:53:53 -0600

    Delete user location document when closing window

--- firebase/locationTracker/js/index.js
+++ firebase/locationTracker/js/index.js
@@ -1,14 +1,21 @@
 import { FirebaseDB } from './firebaseDB.js';
 import util from './util.js';
 
+const firebaseDB = new FirebaseDB();
+firebaseDB.init();
+
 if (!localStorage.getItem('user_id')) {
     localStorage.setItem('user_id', util.uuid());
 }
+const userId = localStorage.getItem('user_id');
+
+window.addEventListener('beforeunload', evt => {
+    firebaseDB.db.collection('user_locations').doc(userId).delete();
+});
 
 if (navigator.geolocation) {
     function sharePosition() {
         navigator.geolocation.getCurrentPosition(pos => {
-            const userId = localStorage.getItem('user_id');
             firebaseDB.db.collection('user_locations').doc(userId).set({
                 lat: pos.coords.latitude,
                 lng: pos.coords.longitude
@@ -21,6 +28,3 @@ if (navigator.geolocation) {
         sharePosition();
     }, 2000);
 }
-
-const firebaseDB = new FirebaseDB();
-firebaseDB.init();
