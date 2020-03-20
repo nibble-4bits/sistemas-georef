fe8bb24ce0ba1c80dd547c1023d9f7a713561fc2
Luis De Anda <ldeandac16@gmail.com>
2020-03-19 23:11:48 -0600

    Remove logic for obtaining geolocation

--- firebase/locationTracker/js/index.js
+++ firebase/locationTracker/js/index.js
@@ -1,37 +1,5 @@
-import { FirebaseDB } from './firebaseDB.js';
 import util from './util.js';
-
-const firebaseDB = new FirebaseDB();
-firebaseDB.init();
 
 if (!localStorage.getItem('user_id')) {
     localStorage.setItem('user_id', util.uuid());
-}
-const userId = localStorage.getItem('user_id');
-
-window.addEventListener('beforeunload', evt => {
-    evt.preventDefault();
-    evt.returnValue = '';
-    firebaseDB.db.collection('user_locations').doc(userId).delete();
-});
-window.addEventListener('unload', evt => {
-    evt.preventDefault();
-    evt.returnValue = '';
-    firebaseDB.db.collection('user_locations').doc(userId).delete();
-});
-
-if (navigator.geolocation) {
-    function sharePosition() {
-        navigator.geolocation.getCurrentPosition(pos => {
-            firebaseDB.db.collection('user_locations').doc(userId).set({
-                lat: pos.coords.latitude,
-                lng: pos.coords.longitude
-            });
-        });
-    }
-
-    sharePosition();
-    setInterval(() => {
-        sharePosition();
-    }, 2000);
 }
