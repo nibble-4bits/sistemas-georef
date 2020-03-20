7ac4f740c8077502ef30f9590c25152cd1da7025
Luis De Anda <ldeandac16@gmail.com>
2020-03-19 23:11:12 -0600

    Change class method to static methods

--- firebase/locationTracker/js/firebaseDB.js
+++ firebase/locationTracker/js/firebaseDB.js
@@ -1,26 +1,19 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCYVULfajsxYfUrUmspWsUo3f7y03T7hjs",
+    authDomain: "location-tracker-65746.firebaseapp.com",
+    databaseURL: "https://location-tracker-65746.firebaseio.com",
+    projectId: "location-tracker-65746",
+    storageBucket: "location-tracker-65746.appspot.com",
+    messagingSenderId: "676486729818",
+    appId: "1:676486729818:web:f1cde92f3889d45a2e8863"
+});
+
+let db = firebase.firestore();
+let markers = {};
+
 class FirebaseDB {
-    constructor() {
-        this.db = null;
-    }
-
-    init() {
-        firebase.initializeApp({
-            apiKey: "AIzaSyCYVULfajsxYfUrUmspWsUo3f7y03T7hjs",
-            authDomain: "location-tracker-65746.firebaseapp.com",
-            databaseURL: "https://location-tracker-65746.firebaseio.com",
-            projectId: "location-tracker-65746",
-            storageBucket: "location-tracker-65746.appspot.com",
-            messagingSenderId: "676486729818",
-            appId: "1:676486729818:web:f1cde92f3889d45a2e8863"
-        });
-
-        this.db = firebase.firestore();
-        this.markers = {};
-        this.__addOnSnapshotListener();
-    }
-
-    __addOnSnapshotListener() {
-        this.db.collection('user_locations').onSnapshot(snapshot => {
+    static addOnSnapshotListener() {
+        db.collection('user_locations').onSnapshot(snapshot => {
             snapshot.docChanges().forEach(change => {
                 const doc = change.doc.data();
                 let coords = new globalThis.google.maps.LatLng(doc.lat, doc.lng);
@@ -42,17 +35,19 @@ class FirebaseDB {
                 });
 
                 if (change.type === 'added') {
-                    this.markers[change.doc] = marker;
+                    markers[change.doc] = marker;
                 }
                 else if (change.type === 'modified') {
-                    this.markers[change.doc].setPosition(coords);
+                    markers[change.doc].setPosition(coords);
                 }
                 else if (change.type === 'removed') {
-                    this.markers[change.doc].setMap(null);
+                    markers[change.doc].setMap(null);
                 }
             });
         });
     }
 }
+
+FirebaseDB.db = db;
 
 export { FirebaseDB };
