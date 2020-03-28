78cf81ed79a1c3d1a805a4c56ba9dba6e10e4af6
LuisZanella <luiscontreraszane@gmail.com>
2020-03-27 22:35:47 -0600

    Divide information from the info window Issue implemented

--- fetch/practica4/index.js
+++ fetch/practica4/index.js
@@ -1,52 +1,81 @@
-'use strict';
+"use strict";
 
-const BASE_API_URL = 'https://corona.lmao.ninja';
+const BASE_API_URL = "https://corona.lmao.ninja";
 
 async function initMap() {
-    const props = {
-        center: {
-            lat: 15,
-            lng: 0
-        },
-        zoom: 2
-    }
-    const map = new google.maps.Map(document.getElementById('map'), props);
-    let globalData = null;
-    let countriesData = null;
+  const props = {
+    center: {
+      lat: 15,
+      lng: 0
+    },
+    zoom: 2
+  };
+  const map = new google.maps.Map(document.getElementById("map"), props);
+  let globalData = null;
+  let countriesData = null;
 
-    $('#modalLoading').modal('show');
-    try {
-        const globalRes = await fetch(`${BASE_API_URL}/all`);
-        globalData = await globalRes.json();
-        cacheAPIData('globalData', globalData);
+  $("#modalLoading").modal("show");
+  try {
+    const globalRes = await fetch(`${BASE_API_URL}/all`);
+    globalData = await globalRes.json();
+    cacheAPIData("globalData", globalData);
+  } catch (error) {
+    globalData = retrieveCachedAPIData("globalData");
+    if (!globalData) {
+      // TODO
     }
-    catch (error) {
-        globalData = retrieveCachedAPIData('globalData');
-        if (!globalData) {
-            // TODO
-        }
-    }
+  }
 
-    try {
-        const countriesRes = await fetch(`${BASE_API_URL}/countries`);
-        countriesData = await countriesRes.json();
-        cacheAPIData('countryData', countriesData);
+  try {
+    const globalRes = await fetch(`${BASE_API_URL}/all`);
+    globalData = await globalRes.json();
+    cacheAPIData("globalData", globalData);
+  } catch (error) {
+    globalData = retrieveCachedAPIData("globalData");
+    if (!globalData) {
+      // TODO
     }
-    catch (error) {
-        countriesData = retrieveCachedAPIData('countryData');
-        if (!countriesData) {
-            // TODO
-        }
+  }
+
+  try {
+    const countriesRes = await fetch(`${BASE_API_URL}/countries`);
+    countriesData = await countriesRes.json();
+    cacheAPIData("countryData", countriesData);
+  } catch (error) {
+    countriesData = retrieveCachedAPIData("countryData");
+    if (!countriesData) {
+      // TODO
     }
-    $('#modalLoading').modal('hide');
+    $("#modalLoading").modal("hide");
 
     updateInfoCards(globalData);
     addCountryMarkers(countriesData, map);
+  }
 }
 
 function generateCountryInfoHTML(country) {
-    return `
+  return `
         <div style="width: 12em;">
+            <div style="display:flex; flex-direction: column; align-items: center; padding-bottom: 1em;">
+                <img src="${country.countryInfo.flag}" alt="Bandera de ${country.country}" style="width: 60%;">
+            </div>
+            <div style="display:flex; flex-direction: column; align-items: center;">
+                <div>
+                    <b>Casos: </b>${country.cases}<br>
+                </div>
+                <div>
+                    <b>Muertes: </b>${country.deaths}<br>
+                </div>
+                <div>
+                    <b>Recuperados: </b>${country.recovered}<br>
+                </div>
+            </div>
+        </div>
+    `;
+}
+function generateFullCountryInfoHTML(country) {
+  return `
+        <div style="width: 12em;" class="googleMapRightControl">
             <div style="display:flex; flex-direction: column; align-items: center; padding-bottom: 1em;">
                 <img src="${country.countryInfo.flag}" alt="Bandera de ${country.country}" style="width: 60%;">
                 <h3 style="margin: 3px 0 0 0;">${country.country}</h3>
@@ -80,55 +109,80 @@ function generateCountryInfoHTML(country) {
         </div>
     `;
 }
+function MakeControl(controlDiv, country) {
+  // Set up the control border.
+  var controlUI = document.createElement("div");
+  controlUI.title = country.country;
+  controlUI.className = "controlUI";
+  controlDiv.appendChild(controlUI);
+
+  // Set up the inner control.
+  var controlText = document.createElement("div");
+  controlText.innerHTML = generateFullCountryInfoHTML(country);
+  controlText.className = "controlText";
+  controlUI.appendChild(controlText);
+}
 
 function cacheAPIData(key, jsonData) {
-    localStorage.setItem(key, JSON.stringify(jsonData));
+  localStorage.setItem(key, JSON.stringify(jsonData));
 }
 
 function retrieveCachedAPIData(key) {
-    return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key));
 }
 
 function updateInfoCards(globalData) {
-    const divLastUpdated = document.getElementById('lastUpdated');
-    const divGlobalConfirmedCases = document.getElementById('globalConfirmedCases');
-    const divGlobalRecovered = document.getElementById('globalRecovered');
-    const divGlobalDeaths = document.getElementById('globalDeaths');
+  const divLastUpdated = document.getElementById("lastUpdated");
+  const divGlobalConfirmedCases = document.getElementById(
+    "globalConfirmedCases"
+  );
+  const divGlobalRecovered = document.getElementById("globalRecovered");
+  const divGlobalDeaths = document.getElementById("globalDeaths");
 
-    divLastUpdated.textContent = new Date(globalData.updated).toLocaleString('es-us', { hour12: true });
-    divGlobalConfirmedCases.textContent = globalData.cases.toLocaleString();
-    divGlobalRecovered.textContent = globalData.recovered.toLocaleString();
-    divGlobalDeaths.textContent = globalData.deaths.toLocaleString();
+  divLastUpdated.textContent = new Date(
+    globalData.updated
+  ).toLocaleString("es-us", { hour12: true });
+  divGlobalConfirmedCases.textContent = globalData.cases.toLocaleString();
+  divGlobalRecovered.textContent = globalData.recovered.toLocaleString();
+  divGlobalDeaths.textContent = globalData.deaths.toLocaleString();
 }
 
 function addCountryMarkers(countriesData, map) {
-    let arrInfoWindows = [];
-    const icon = {
-        url: 'https://image.flaticon.com/icons/png/128/2659/2659980.png',
-        scaledSize: new google.maps.Size(24, 24),
-        origin: new google.maps.Point(0, 0)
-    };
+  let arrInfoWindows = [];
+  const icon = {
+    url: "https://image.flaticon.com/icons/png/128/2659/2659980.png",
+    scaledSize: new google.maps.Size(24, 24),
+    origin: new google.maps.Point(0, 0)
+  };
 
-    for (const country of countriesData) {
-        const info = generateCountryInfoHTML(country);
-        const infoWindow = new google.maps.InfoWindow({
-            content: info
-        });
+  for (const country of countriesData) {
+    const info = generateCountryInfoHTML(country);
+    const infoWindow = new google.maps.InfoWindow({
+      content: info
+    });
 
-        const marker = new google.maps.Marker({
-            map: map,
-            icon: icon,
-            position: new google.maps.LatLng(country.countryInfo.lat, country.countryInfo.long),
-            title: `${country.country}`
-        });
+    const marker = new google.maps.Marker({
+      map: map,
+      icon: icon,
+      position: new google.maps.LatLng(
+        country.countryInfo.lat,
+        country.countryInfo.long
+      ),
+      title: `${country.country}`
+    });
 
-        marker.addListener('click', () => {
-            for (const infoWin of arrInfoWindows) {
-                infoWin.close();
-            }
-            infoWindow.open(map, marker);
-        });
+    marker.addListener("click", () => {
+      for (const infoWin of arrInfoWindows) {
+        infoWin.close();
+        if (map.controls[google.maps.ControlPosition.RIGHT_CENTER].length != 0)
+          map.controls[google.maps.ControlPosition.RIGHT_CENTER].pop();
+      }
+      infoWindow.open(map, marker);
+      var divName = document.createElement("div");
+      new MakeControl(divName, country);
+      map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(divName);
+    });
 
-        arrInfoWindows.push(infoWindow);
-    }
+    arrInfoWindows.push(infoWindow);
+  }
 }
