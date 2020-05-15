ac23f32d8f7547475230662680ca262c3dded650
Luis De Anda <ldeandac16@gmail.com>
2020-05-14 21:54:51 -0500

    Create constants.js file

---
+++ firebase/practica4/js/constants.js
@@ -0,0 +1,9 @@
+// auth.js
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+const btnSalir = document.getElementById('btnSalir');
+
+// Index.js
+const accountInfo = document.getElementById('accountInfo');
+const mapContainer = document.getElementById('map');
+const gmaps = document.getElementById('gmapsScript');
